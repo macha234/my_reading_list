d3b3dc8480c913692d9f535d8c5e9cafd149b73e
basil <machariabasi@gmail.com>
2024-08-08 12:15:49 +0300

    adding_a_book

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,33 @@ import "./index.css"
 
 
 function App() {
+const [nameOfBook,setNameOfBook] = useState("")
+const [startingDate, setStartingDate] = useState("")
+const [endDate, setEndDate] = useState("")
+
+function onSubmit(event){
+  // prevents the default behaviour of reloading the page
+  event.preventDefault()
+
+  // construct data as an object
+  const data = {
+    id: Math.random(),
+    name: nameOfBook,
+    startingDate: startingDate,
+    endDate: endDate,
+  }
+  // make a copy of the array(books)
+  const NewData =[...books]
+
+  // push data to the array(newData)
+  NewData.push(data)
+
+  // set newData to books 
+  setBooks(NewData)
+}
+
+
+
   const [books, setBooks] = useState([
     {
       id: 1,
@@ -23,18 +50,42 @@ function App() {
       name: "Jack and the bean stalk",
       startingDate: "10/7/24",
       endDate: "13/7/24"
-     }
+     },
+   
+
   ])
   
   return (
     <>
     <h1 className="title">MY READING LIST</h1>
-    {books.map(book =>(
+ 
+ <div className="wrapper">
+ <div>
+ {books.map(book =>(
       <Book
       book={book}
       books={books}
       setBooks={setBooks} />
     ))}
+ </div>
+
+
+   <div>
+    <h3>ADD A BOOK</h3>
+    
+   <form onSubmit={onSubmit}>
+      <input type="text" value={nameOfBook}  onChange={(event) => setNameOfBook(event.target.value)} placeholder="Name of book"/> 
+      <br />
+      <input type="text" value={startingDate} onChange={(event) => setStartingDate(event.target.value)} placeholder="Starting date"/>
+      <br />
+      <input type="text" value={endDate} onChange={(event) => setEndDate(event.target.value)} placeholder="End date"/>
+<br />
+
+      <button>submit</button>
+      
+    </form>
+   </div>
+ </div>
       
     </>
   )
